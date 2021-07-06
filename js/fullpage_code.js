b72c2874116c06ad12c85a90e4a5f76c6f9502f1
ssongd-creater <sksk1440@gmail.com>
2021-07-06 18:30:29 +0900

    7/6 mammoth fullpage plug-in 적용

---
+++ js/fullpage_code.js
@@ -0,0 +1,6 @@
+$(function () {
+  $('#fullpage').fullpage({
+    anchors : ['1st','2nd','3rd'],
+  });
+
+});
--- js/jq.main.js
+++ js/jq.main.js
@@ -21,5 +21,52 @@ $(function () {
     $(".subnav_box").hide();
     $(".subnav_box").eq(i).show();
   });
+
+  // Mobile Menu Animation Effect
+  $(".menu_ico").hover(function () {
+    $(this).toggleClass('active');
+  });
+
+  //Mobile Menu Click Effect
+  $(".menu_ico").click(function () {
+    $(".mobile_nav").animate({ left: 0 }, 300);
+    $("section").animate({ left: '60%' }, 300);
+    $(".overlay").animate({
+      "opacity": 0.7,
+      "left": "60%",
+      "width":"100%"
+    },300);
+  });
+
+  $(".close_btn, .overlay").click(function () {
+    $(".mobile_nav").animate({ left: '-60%' }, 300);
+    $("section").animate({ left: 0 }, 300);
+    $(".overlay").animate({
+      "opacity": 0,
+      "left": 0,
+      "width":0
+    },300);
+  });
+
+  // Mobile Menu Accordion Effect
+  $(".mobile_tit").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).siblings(".mobile_sub_nav").slideUp(200);
+      $(this).removeClass("rotate");
+      $(".mobile_tit").removeClass("rotate");
+
+    } else {
+      $(".mobile_tit").removeClass("active");
+      //mobile_tit의 active를 모두 지운후
+      $(".motile_tit").removeClass("rotate");
+      $(this).addClass("active");
+      //내가 선택한 this의 active를 주는것
+      $(".mobile_sub_nav").slideUp(200);
+      $(this).siblings(".mobile_sub_nav").slideDown(200);
+      $(this).addClass("rotate");
+      
+    }
+  });
 });
 

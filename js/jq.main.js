84487b1580bfbbc8847211a3f6c4350124e16986
ssongd-creater <sksk1440@gmail.com>
2021-07-09 10:04:32 +0900

    7/8 renewal_mammoth 완성

--- js/jq.main.js
+++ js/jq.main.js
@@ -59,7 +59,7 @@ $(function () {
     } else {
       $(".mobile_tit").removeClass("active");
       //mobile_tit의 active를 모두 지운후
-      $(".motile_tit").removeClass("rotate");
+      $(".mobile_tit").removeClass("rotate");
       $(this).addClass("active");
       //내가 선택한 this의 active를 주는것
       $(".mobile_sub_nav").slideUp(200);
@@ -68,5 +68,63 @@ $(function () {
       
     }
   });
+
+  //Slide Functions
+  function resSize(autoLength) {
+    const liWidth = $(".slide_li").width() * autoLength;
+    const liLength = $(".slide_li").width() * $(".slide_li").length;
+    $(".slide_box, .slides").width(liWidth);
+    $(".slide_inner").width(liLength);
+  }
+  
+  $(window).resize(function () {
+    const wWidth = $(window).width();
+    if (wWidth >= 700) {
+      resSize(3);
+    } else if (wWidth < 700) {
+      resSize(2);
+    };
+  });
+
+  if (matchMedia("screen and (min-width:900px").matches) {
+    resSize(3);
+  } else if (matchMedia("screen and (max-width:899px").matches) {
+    resSize(2);
+  }
+  
+  function slideInit() {
+    let slideCount = 0;
+    const liWidth = $(".slide_li").width();
+    const slideSize = $(".slide_box").width() / $(".slide_li").width(); //6
+    //const slideSize = $(".slide_box").width() 이부분은 slide가 보여질 수 있는 공간박스
+    const slideLength = $(".slide_li").length - slideSize;//3
+    //console.log(slideSize); --4 / 1050
+    //console.log(slideLength); --1 / 350
+ 
+
+    $(".arr_next").click(function () {
+      slideCount++;
+      if (slideCount > slideLength) {
+        slideCount = 0;
+      }
+      $(".slide_inner").stop().animate({"margin-left": slideCount * -liWidth}, 250);
+    });
+
+    
+    $(".arr_prev").click(function () {
+      slideCount--;
+      if (slideCount < 0) {
+        slideCount = slideLength;
+      }
+      $(".slide_inner").stop().animate({"margin-left": slideCount * -liWidth}, 250);
+    });
+  }
+
+  $(window).resize(function () {
+    $(".slide_inner").stop().animate({ "margin-left": 0 }, 100);
+    slideInit();
+  });
+  slideInit();
+
 });
 
